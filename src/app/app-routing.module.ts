453953ce2de168f0e6447e37e07d3d42e9960df7
yarob <yalrifai41@gmail.com>
2024-06-18 17:57:44 +0300

    adding custom preload service and inject it in the app routing file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { CustomPreloadingService } from './services/custom-preloading.service';
 
 const routes: Routes = [
   {
@@ -13,11 +14,30 @@ const routes: Routes = [
   //   loadChildren: () =>
   //     import('./pages/users/users.module').then((m) => m.UserModule),
   // },
+  // green this for use custom preloding strategy
+  // {
+  //   path: 'products',
+  //   loadChildren: () =>
+  //     import('./products/products.module').then((m) => m.ProductsModule),
+  //   data: { preload: false }, // Do not preload this module
+  // },
+  // green this for use custom preloding strategy
+  // {
+  //   path: 'another-feature',
+  //   loadChildren: () =>
+  //     import('./products/products.module').then((m) => m.ProductsModule),
+  //   data: { preload: true }, //preload this module
+  // },
 ];
 
 @NgModule({
   imports: [
+    // green this for use preload all
     RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
+    // green this for use custom preloding strategy
+    // RouterModule.forRoot(routes, {
+    //   preloadingStrategy: CustomPreloadingService,
+    // }),
   ],
   exports: [RouterModule],
 })
--- src/app/services/custom-preloading.service.ts
+++ src/app/services/custom-preloading.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
-import { Route } from '@angular/router';
+import { PreloadingStrategy, Route } from '@angular/router';
 import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class CustomPreloadingServicePreloadingStrategy {
+export class CustomPreloadingService  implements PreloadingStrategy {
   preload(route: Route, load: () => Observable<any>): Observable<any> {
     if (route.data && route.data['preload']) {
       return load();
